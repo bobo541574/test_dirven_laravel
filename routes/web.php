dda5b4ae6e1e02463d6e484645714d33dbd0e830
bobo541574 <bobo541574.dev@gmail.com>
2020-11-29 11:03:36 +0630

    PHPUnit Setup & Books Test Part-1

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,7 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('/books', 'BookController');
+// Route::post('/books', 'BookController@store');
+// Route::patch('/books', 'BookController@update');
